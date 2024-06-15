ab89409928a1d9db0f516eb4f6bf8a1544a250ca
avidrucker <avidrucker@gmail.com>
2024-06-15 00:26:06 -0400

    clean up unused files

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   base: '/anki-card-test-1/',
   build: {
-    outDir: 'build/assets', // Make sure the output directory aligns with what gh-pages expects
-    assetsDir: 'assets'
+    outDir: 'build/assets' // Make sure the output directory aligns with what gh-pages expects
   },
   publicDir: 'public',
   plugins: [react()],
