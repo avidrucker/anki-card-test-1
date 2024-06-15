24e7846caa0cc4102f6f08a295bc403be1618f8a
avidrucker <avidrucker@gmail.com>
2024-06-14 23:13:11 -0400

    publish to gh pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/anki-card-test-1/',
+  build: {
+    outDir: 'build' // Make sure the output directory aligns with what gh-pages expects
+  },
   plugins: [react()],
-})
+});
