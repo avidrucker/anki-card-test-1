b881020f648c4ae034934307617c39538347f21a
avidrucker <avidrucker@gmail.com>
2024-06-15 00:23:21 -0400

    fix reconfig vite config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   base: '/anki-card-test-1/',
   build: {
-    outDir: 'build', // Make sure the output directory aligns with what gh-pages expects
+    outDir: 'build/assets', // Make sure the output directory aligns with what gh-pages expects
     assetsDir: 'assets'
   },
   publicDir: 'public',
