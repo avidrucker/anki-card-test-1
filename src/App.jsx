712126d96fa46473e91b55758eb3ae97215d2468
avidrucker <avidrucker@gmail.com>
2024-06-29 21:52:33 -0400

    feat editor display toggle

--- src/App.jsx
+++ src/App.jsx
@@ -19,6 +19,7 @@ import 'highlight.js/styles/monokai-sublime.css';
 const BTN_STYLE = "bn mr1 f6 link dim br3 w35 pv2 dib pointer bg-gray";
 const NARROW_BTN_STYLE = "bn f6 link dim br3 ph2 pv2 ml1 dib pointer bg-gray white";
 const BTN_STYLE_GLASS = "link ba b--gray bw1 br3 dib pa2 w2 h2 pointer";
+const BTN_STYLE_TOGGLE = "link ba b--gray f6 bw1 br3 w2 dib pa2 pointer transparent-btn"
 const ACTIVE_BTN_STYLE = "fw6 white";
 const INACTIVE_BTN_STYLE = "light-gray";
 const CARD_STYLE = "ba b--black-10";
@@ -26,6 +27,8 @@ const CARD_STYLE = "ba b--black-10";
 // <!-- Font Awesome Free 5.15.4 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free (Icons: CC BY 4.0, Fonts: SIL OFL 1.1, Code: MIT License) -->
 const copyIcon = <svg xmlns="http://www.w3.org/2000/svg" fill="gray" viewBox="0 0 448 512"><path d="M433.941 65.941l-51.882-51.882A48 48 0 0 0 348.118 0H176c-26.51 0-48 21.49-48 48v48H48c-26.51 0-48 21.49-48 48v320c0 26.51 21.49 48 48 48h224c26.51 0 48-21.49 48-48v-48h80c26.51 0 48-21.49 48-48V99.882a48 48 0 0 0-14.059-33.941zM266 464H54a6 6 0 0 1-6-6V150a6 6 0 0 1 6-6h74v224c0 26.51 21.49 48 48 48h96v42a6 6 0 0 1-6 6zm128-96H182a6 6 0 0 1-6-6V54a6 6 0 0 1 6-6h106v88c0 13.255 10.745 24 24 24h88v202a6 6 0 0 1-6 6zm6-256h-64V48h9.632c1.591 0 3.117.632 4.243 1.757l48.368 48.368a6 6 0 0 1 1.757 4.243V112z"/></svg>;
 const checkIcon = <svg xmlns="http://www.w3.org/2000/svg" fill="gray" viewBox="0 0 512 512"><path d="M173.898 439.404l-166.4-166.4c-9.997-9.997-9.997-26.206 0-36.204l36.203-36.204c9.997-9.998 26.207-9.998 36.204 0L192 312.69 432.095 72.596c9.997-9.997 26.207-9.997 36.204 0l36.203 36.204c9.997 9.997 9.997 26.206 0 36.204l-294.4 294.401c-9.998 9.997-26.207 9.997-36.204-.001z"/></svg>
+const eyeIcon = <svg xmlns="http://www.w3.org/2000/svg" fill="gray" viewBox="0 0 576 512"><path d="M572.52 241.4C518.29 135.59 410.93 64 288 64S57.68 135.64 3.48 241.41a32.35 32.35 0 0 0 0 29.19C57.71 376.41 165.07 448 288 448s230.32-71.64 284.52-177.41a32.35 32.35 0 0 0 0-29.19zM288 400a144 144 0 1 1 144-144 143.93 143.93 0 0 1-144 144zm0-240a95.31 95.31 0 0 0-25.31 3.79 47.85 47.85 0 0 1-66.9 66.9A95.78 95.78 0 1 0 288 160z"/></svg>;
+const eyeSlashIcon = <svg xmlns="http://www.w3.org/2000/svg" fill="gray" viewBox="0 0 640 512"><path d="M320 400c-75.85 0-137.25-58.71-142.9-133.11L72.2 185.82c-13.79 17.3-26.48 35.59-36.72 55.59a32.35 32.35 0 0 0 0 29.19C89.71 376.41 197.07 448 320 448c26.91 0 52.87-4 77.89-10.46L346 397.39a144.13 144.13 0 0 1-26 2.61zm313.82 58.1l-110.55-85.44a331.25 331.25 0 0 0 81.25-102.07 32.35 32.35 0 0 0 0-29.19C550.29 135.59 442.93 64 320 64a308.15 308.15 0 0 0-147.32 37.7L45.46 3.37A16 16 0 0 0 23 6.18L3.37 31.45A16 16 0 0 0 6.18 53.9l588.36 454.73a16 16 0 0 0 22.46-2.81l19.64-25.27a16 16 0 0 0-2.82-22.45zm-183.72-142l-39.3-30.38A94.75 94.75 0 0 0 416 256a94.76 94.76 0 0 0-121.31-92.21A47.65 47.65 0 0 1 304 192a46.64 46.64 0 0 1-1.54 10l-73.61-56.89A142.31 142.31 0 0 1 320 112a143.92 143.92 0 0 1 144 144c0 21.63-5.29 41.79-13.9 60.11z"/></svg>;
 
 const availableDesigns = ["8 Bit Console.json", 
                           "Blackboard and Chalk.json", 
@@ -55,6 +58,7 @@ function App() {
   const [cardData, setCardData] = useState({});
   const [previewData, setPreviewData] = useState("");
   const [cardIndex, setCardIndex] = useState(0);
+  const [editorViewCollapsed, setEditorViewCollapsed] = useState(false);
 
   useEffect(() => {
     if (isEditing) {
@@ -384,6 +388,10 @@ function App() {
   const handleBlur = async () => {
     await prettify();
   };
+
+  const handleToggleEditorView = () => {
+    setEditorViewCollapsed(!editorViewCollapsed);
+  }
   
 
   const loadDesign = (filename) => {
@@ -419,7 +427,9 @@ function App() {
     <div className="App w-100 flex flex-column vh-100 pb2">
       <header className="flex flex-column flex-row-ns justify-between items-center pb2">{/*responsive design test classes: bg-blue bg-red-m bg-purple-l*/}
         <div className="header-left-side w-100 flex flex-column items-center-ns flex-row-ns">
-          <h1 className="ma0 pv2 ph2 dib w-100 tc tl-ns">Card Designer</h1>
+          <h1 className="ma0 pv2 ph2 dib w-100 tc tl-ns">
+            <a className="link white f2" href="https://avidrucker.github.io/anki-card-test-1/">Card Designer</a>
+          </h1>
           {editingName ?
             <input 
               ref={nameInputRef} 
@@ -431,12 +441,15 @@ function App() {
               onBlur={finalizeDesignName}
               onKeyDown={(e) => { if (e.key === 'Enter') setEditingName(false); }}
             /> :
-            <h2 className="ma0 pv1 ph2 dib pointer f3 ba w-100 tc tl-ns b--white bg-dark-gray" onClick={() => setEditingName(true)}>{designName}</h2>
+            <h2 title="Click here to name your design" className="ma0 pv1 ph2 dib pointer f3 ba w-100 tc tl-ns b--white bg-dark-gray" onClick={() => setEditingName(true)}>{designName}</h2>
           }
         </div>
         <div className="header-right-side w-100 ph2 flex flex-column-reverse flex-row-l">
 
           <div className="card-navigation pr2-l dib ml-auto mr-auto mr0-ns mt2 mt1-m mt0-l mr2 flex items-center">
+            <button title={editorViewCollapsed ? "Show Editor" : "Hide Editor"} className={`${BTN_STYLE_TOGGLE} relative`} onClick={handleToggleEditorView}>
+              <span className="relative flex flex-column justify-center items-center">{editorViewCollapsed ? eyeSlashIcon : eyeIcon}</span>{/*shift-up-right*/}
+            </button>
             <button className={`${NARROW_BTN_STYLE}`} title="Previous" onClick={handlePreviousCard}>&lt;</button>
             <p className="ma0 ml1 prevent-select w3 f5 tc">{`Data ${cardIndex + 1}`}</p>
             <button className={`${NARROW_BTN_STYLE}`} title="Next" onClick={handleNextCard}>&gt;</button>
@@ -464,8 +477,8 @@ function App() {
           </div>
         </div>
         </header>
-      <div className="workspace flex flex-column-reverse flex-row-ns flex-auto-ns ph2">
-        <div className="editor w-100 w-50-ns flex flex-column pr1">
+      <div className={`workspace flex flex-column-reverse flex-row-ns flex-auto-ns ph2`}>
+        <div className={`editor w-100 w-50-ns flex flex-column w-animate pr1 overflow-x-hidden ${(editorViewCollapsed && " w0-ns-strong ")}`}>
           <div className="tabs">
             <button onClick={() => handleTabChange('frontHtml')} 
               className={activeTab === 'frontHtml' ? 
@@ -511,7 +524,7 @@ function App() {
             )}
           </div>
         </div>
-        <div className="card-display pb2 pb0-ns w-100 w-50-ns flex flex-column pl1">
+        <div className={`card-display pb2 pb0-ns w-100 w-50-ns flex w-animate flex-column pl1 ${(editorViewCollapsed && " w-100-ns-strong ")}`}>
           <div className="view-tabs">
             <button onClick={() => handleViewChange('front')} 
               className={viewSide === 'front' ? 
